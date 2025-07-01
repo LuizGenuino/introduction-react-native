ec7489ea57e89e4afb416931a1d67f6888e2b25c
LuizGenuino <luiz.genuino.dev@gmail.com>
2025-07-01 10:19:18 -0400

    feat: cria pagina principal e componente de botão

---
+++ src/app/index.tsx
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet, Alert } from "react-native"
+import { Button } from "../components/button/Index"
+
+export default function Index(){
+
+    function handleMessage(){
+        return Alert.alert("Usando Alerta!")
+    }
+
+    return (
+        <View style={styles.container} >
+            <Text style={styles.title} >Hello World!</Text>
+            <Button />
+            <Button />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 32,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 16
+    },
+
+    title: {
+        color: "red",
+        fontSize: 24,
+        fontWeight: "bold"
+    }
+})
---
+++ src/components/button/Index.tsx
@@ -0,0 +1,11 @@
+import { Text, TouchableOpacity } from "react-native";
+
+import {styles} from "./styles"
+
+export function Button(){
+    return (
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.title} >Clique Aqui</Text>
+        </TouchableOpacity>
+    )
+}
